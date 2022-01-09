938756dc3cf03b0ce2487f22138983a52ef75dbe
Souria-saky HOCQUENGHEM <shocquen@student.42.fr>
2022-01-09 16:46:10 +0100

    real tests

--- framework/real-tests/00_gnl_test.c
+++ framework/real-tests/00_gnl_test.c
@@ -1,22 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   00_gnl_tests.c                                     :+:      :+:    :+:   */
+/*   00_gnl_test.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: shocquen <shocquen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/09 14:57:35 by shocquen          #+#    #+#             */
-/*   Updated: 2022/01/09 14:58:37 by shocquen         ###   ########.fr       */
+/*   Updated: 2022/01/09 15:44:32 by shocquen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libunit.h"
-#include "get_next_line.h"
+#include "gnl_test.h"
 
 int	gnl_launcher(void)
 {
 	t_unit_test	*tests;
 
-	load_test();
+	load_test(&tests, "1st line", "GNL", &gnl_test_01);
 	return (launch_tests(&tests));
 }
